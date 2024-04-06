2991f55d7f76f0b885220decf1c96fa0fa9aa0b7
zachcyn <zachchuyienian@gmail.com>
2024-04-06 23:25:07 +0100

    Update on 2024-04-06

--- codes/UI/src/components/mode-button/index.jsx
+++ codes/UI/src/components/mode-button/index.jsx
@@ -5,63 +5,55 @@ import { styled, useTheme } from '@mui/material/styles';
 
 import { ThemeContext } from 'src/theme';
 
+// Define IOSSwitch outside the component to avoid re-definition on each render
+const IOSSwitch = styled(Switch)(({ theme }) => ({
+  width: 42,
+  height: 26,
+  padding: 0,
+  '& .MuiSwitch-switchBase': {
+    padding: 0,
+    margin: 2,
+    transitionDuration: '300ms',
+    '&.Mui-checked': {
+      transform: 'translateX(16px)',
+      color: theme.palette.text.primary,
+      '& .MuiSwitch-thumb': {
+        backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 20 20"><path fill="${encodeURIComponent(
+          '#000',
+        )}" d="M4.2 2.5l-.7 1.8-1.8.7 1.8.7.7 1.8.6-1.8L6.7 5l-1.9-.7-.6-1.8zm15 8.3a6.7 6.7 0 11-6.6-6.6 5.8 5.8 0 006.6 6.6z"/></svg>')`,
+      },
+      '& + .MuiSwitch-track': {
+        backgroundColor: theme.palette.background.default,
+        opacity: 1,
+        border: 0,
+      },
+    },
+  },
+  '& .MuiSwitch-thumb': {
+    boxSizing: 'border-box',
+    backgroundColor: theme.palette.mode === 'light' ? theme.palette.background.default:theme.palette.grey[400],
+    backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 20 20"><path fill="${encodeURIComponent(
+      '#000000',
+    )}" d="M9.305 1.667V3.75h1.389V1.667h-1.39zm-4.707 1.95l-.982.982L5.09 6.072l.982-.982-1.473-1.473zm10.802 0L13.927 5.09l.982.982 1.473-1.473-.982-.982zM10 5.139a4.872 4.872 0 00-4.862 4.86A4.872 4.872 0 0010 14.862 4.872 4.872 0 0014.86 10 4.872 4.872 0 0010 5.139zm0 1.389A3.462 3.462 0 0113.471 10a3.462 3.462 0 01-3.473 3.472A3.462 3.462 0 016.527 10 3.462 3.462 0 0110 6.528zM1.665 9.305v1.39h2.083v-1.39H1.666zm14.583 0v1.39h2.084v-1.39h-2.084zM5.09 13.928L3.616 15.4l.982.982 1.473-1.473-.982-.982zm9.82 0l-.982.982 1.473 1.473.982-.982-1.473-1.473zM9.305 16.25v2.083h1.389V16.25h-1.39z"/></svg>')`,
+    width: 22,
+    height: 22,
+  },
+  '& .MuiSwitch-track': {
+    borderRadius: 26 / 2,
+    backgroundColor: theme.palette.mode === 'light' ? theme.palette.grey[400]:theme.palette.background.default,
+    opacity: 1,
+    transition: theme.transitions.create(['background-color'], {
+      duration: 500,
+    }),
+  },
+}));
+
 const ThemeToggleButton = () => {
-  const { toggleTheme } = useContext(ThemeContext);
+  const { toggleTheme, mode } = useContext(ThemeContext);
   const theme = useTheme();
 
-  const IOSSwitch = styled((props) => (
-      <Switch focusVisibleClassName=".Mui-focusVisible" disableRipple {...props} />
-    ))(({ theme }) => ({
-      width: 42,
-      height: 26,
-      padding: 0,
-      '& .MuiSwitch-switchBase': {
-        padding: 0,
-        margin: 2,
-        transitionDuration: '300ms',
-        '&.Mui-checked': {
-          transform: 'translateX(16px)',
-          color: '#fff',
-          '& + .MuiSwitch-track': {
-            backgroundColor: theme.palette.mode === 'dark' ? '#2ECA45' : '#65C466',
-            opacity: 1,
-            border: 0,
-          },
-          '&.Mui-disabled + .MuiSwitch-track': {
-            opacity: 0.5,
-          },
-        },
-        '&.Mui-focusVisible .MuiSwitch-thumb': {
-          color: '#33cf4d',
-          border: '6px solid #fff',
-        },
-        '&.Mui-disabled .MuiSwitch-thumb': {
-          color:
-            theme.palette.mode === 'light'
-              ? theme.palette.grey[100]
-              : theme.palette.grey[600],
-        },
-        '&.Mui-disabled + .MuiSwitch-track': {
-          opacity: theme.palette.mode === 'light' ? 0.7 : 0.3,
-        },
-      },
-      '& .MuiSwitch-thumb': {
-        boxSizing: 'border-box',
-        width: 22,
-        height: 22,
-      },
-      '& .MuiSwitch-track': {
-        borderRadius: 26 / 2,
-        backgroundColor: theme.palette.mode === 'light' ? '#E9E9EA' : '#39393D',
-        opacity: 1,
-        transition: theme.transitions.create(['background-color'], {
-          duration: 500,
-        }),
-      },
-    }));
-
   return (
-    <IOSSwitch sx={{ m: 1 }} onClick={toggleTheme}/>
+    <IOSSwitch sx={{ m: 1 }} onClick={() => toggleTheme(mode === 'light' ? 'dark' : 'light')} theme={theme} checked={mode === 'dark'}/> 
   );
 };
 
